0f6b778bd75a3aebc27f03649bf5eeedc7b006e8
octaltree <octaltree@gmail.com>
2016-04-02 15:10:49 +0900

    main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from flask import Flask
+from flask import request
+import tweepy
+
+# undefined :: a
+undefined = None
+
+#CONSUMER_KEY = ''
+#CONSUMER_SECRET = ''
+#ACCESS_TOKEN = ''
+#ACCESS_SECRET = ''
+#
+#auth = undefined
+#auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+#
+#tw = tweepy.API(auth)
+app = Flask(__name__)
+
+@app.route('/imgs', methods=['GET', 'POST'])
+def imgs():
+    def post(req):
+        print(req.headers['Content-Type'])
+        return req.data
+    if request.method == 'POST': return post(request)
+    else: return 'post image, return "pic.twitter.com/.*"'
+
+def postImgRespondUrl():
+    return undefined
+
+if __name__ == "__main__" :
+    exit(app.run(debug=True))
+
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et fdm=indent fdl=0 fdn=1:
+# vim:cinw=if,elif,else,for,while,try,except,finally,def,class:
